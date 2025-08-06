3521a942bc22277549d021ae028ba95259b7c217
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-08-06 21:17:22 +0530

    Add files via upload

--- JavaScript/Ping_Pong/index.js
+++ JavaScript/Ping_Pong/index.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
    var ball=document.getElementById("ball");
    var paddle=document.getElementById("paddle");
+   var table=document.getElementById("ping-pong")
     let ballx=50;
     let bally=50;
     ball.style.left=`${ballx}px`;
@@ -14,11 +15,32 @@ document.addEventListener("DOMContentLoaded",()=>{
     bally+=dy;
        ball.style.left=`${ballx}px`;
        ball.style.top=`${bally}px`;
+
+      if(ballx<paddle.offsetLeft+paddle.offsetWidth 
+         && bally>paddle.offsetTop  
+         && bally+ball.offsetHeight<paddle.offsetTop+paddle.offsetHeight){
+         dx*=-1;
+      }
+
        if(ballx>680 || ballx<=0) dx*=(-1);
        if(bally>380 || bally<=0) dy*=(-1);
 
-    //    bally+=dy;
-    //    ball.style.top=`${bally}px`;
+
+   
+    //collision of ball and paddle
 
     },1) 
+
+    var paddley=0;
+    let dpy=10;
+    document.addEventListener("keydown",(event)=>{
+      event.preventDefault();
+      if(event.keyCode==40 && paddley<table.offsetHeight-paddle.offsetHeight){
+         paddley+=dpy;
+      }
+      else if(event.keyCode==38 && paddley>0){
+        paddley+=(-1)*dpy;
+      }
+      paddle.style.top=`${paddley}px`
+    })
 })
